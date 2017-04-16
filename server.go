cbc18868100d25c4de0909eea791666904363baa
Marc CB <marc.cbrooks@gmail.com>
2017-04-16 13:40:37 -0400

    initial commit

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	// create Gorilla mux router
+	r := mux.NewRouter()
+
+	// list routes here
+
+	// use negroni to serve static files
+	n := negroni.New(negroni.NewStatic(http.Dir("./static/")))
+	n.UseHandler(r)
+	log.Fatal(http.ListenAndServe(":3000", n))
+}
